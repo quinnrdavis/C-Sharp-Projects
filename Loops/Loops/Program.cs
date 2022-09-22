8c91fbb0da0bf8c6ea7fde68db793b7ea6b1d90b
Quinn R Davis <quinn.r.davis@gmail.com>
2022-09-22 15:44:38 -0700

    Loops assignment

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // initialize both variables
+            int num1 = 0;
+            int num2 = 2;
+
+            // while loop that prints out numbers 0 through 10 and stops at 10
+            while (num1 <= 10)
+            {
+                Console.WriteLine(num1);
+                num1++;
+            }
+
+            // do while loop that prints successive powers of 2 until the next one would be greater than 20000
+            do
+            {
+                Console.WriteLine(num2);
+                num2 += num2;
+            }
+            while (num2 < 20000);
+            Console.Read();
+        }
+    }
+}
