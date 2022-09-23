4764df9de850846533c5622e9a151260e959349e
Quinn R Davis <quinn.r.davis@gmail.com>
2022-09-23 15:43:50 -0700

    Methods assignment

---
+++ Methods/Methods/MethodClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class MethodClass
+    {
+        public int Addition(int i)
+        {
+            return i + 5;
+        }
+
+        public int Subtraction(int i)
+        {
+            return i - 5;
+        }
+
+        public int Multiplication(int i)
+        {
+            return i * 5;
+        }
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ask for input
+            Console.WriteLine("Please input a number:");
+            // cast input to int
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            // instantiate object
+            MethodClass methods = new MethodClass();
+
+            // print result of each method
+            Console.WriteLine(methods.Addition(input) + "\n" + methods.Subtraction(input) + "\n" + methods.Multiplication(input));
+
+            Console.Read();
+        }
+    }
+}
