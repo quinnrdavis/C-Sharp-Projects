97dc780731331fd2ae0ffa1d55965b922ce3f2fd
Quinn R Davis <quinn.r.davis@gmail.com>
2022-09-26 14:59:18 -0700

    Operator overload assignment

---
+++ OperatorOverload/OperatorOverload/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverload
+{
+    public class Employee
+    {
+        // properties
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        // override ==, return true or false
+        public static bool operator== (Employee e1, Employee e2)
+        {
+            return e1.Id == e2.Id;
+        }
+
+        // override !=, return true or false
+        public static bool operator!= (Employee e1, Employee e2)
+        {
+            return e1.Id != e2.Id;
+        }
+    }
+}
---
+++ OperatorOverload/OperatorOverload/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate two employee objects with different names but the same id
+            Employee e1 = new Employee() { Id = 1, firstName="Quinn" };
+            Employee e2 = new Employee() { Id = 1, firstName="Bob" };
+
+            // print results of overloaded comparisons
+            Console.WriteLine(e1 == e2);
+            Console.WriteLine(e1 != e2);
+
+            Console.ReadLine();
+
+        }
+    }
+}
