c5efae86e7bf326a0d20e93c0037cfdec64a11ac
Quinn R Davis <quinn.r.davis@gmail.com>
2022-09-26 10:50:11 -0700

    Added user input

--- Classes/Classes/Program.cs
+++ Classes/Classes/Program.cs
@@ -13,10 +13,16 @@ namespace Classes
             // instantiate class
             Class1 class1 = new Class1();
 
+            // get input from user
+            Console.WriteLine("Please enter a number: ");
+
+            // cast input to int
+            int input = Convert.ToInt32(Console.ReadLine());
+
             // declare variable to hold output parameter
             int output;
             // call method, store output in output variable
-            class1.Method1(6, out output);
+            class1.Method1(input, out output);
 
             // print output parameter
             Console.WriteLine(output);
