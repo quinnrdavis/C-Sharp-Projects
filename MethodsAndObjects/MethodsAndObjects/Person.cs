e8e7199e1b90c1e961075452e0062a69ce8f196b
Quinn R Davis <quinn.r.davis@gmail.com>
2022-09-26 11:11:11 -0700

    Methods and objects inheritance assignment

---
+++ MethodsAndObjects/MethodsAndObjects/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    class Person
+    {
+        // first name and last name properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // print full name to screen
+        public void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodsAndObjects/MethodsAndObjects/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate employee class
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+
+            // call superclass method
+            employee.SayName();
+        }
+    }
+}
