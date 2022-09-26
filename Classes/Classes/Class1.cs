27258554e73e3fac6fb74c27663b2f90d501d9b9
Quinn R Davis <quinn.r.davis@gmail.com>
2022-09-26 10:48:32 -0700

    Class assignment

---
+++ Classes/Classes/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Class1
+    {
+        // void method with output parameter
+        public void Method1(int i, out int output)
+        {
+            output = i / 2;
+        }
+
+        // overloaded method with output parameter
+        public void Method1(int i, out int output, int j = 1)
+        {
+            output = i + j;
+        }
+    }
+}
---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate class
+            Class1 class1 = new Class1();
+
+            // declare variable to hold output parameter
+            int output;
+            // call method, store output in output variable
+            class1.Method1(6, out output);
+
+            // print output parameter
+            Console.WriteLine(output);
+
+            Console.ReadLine();
+        }
+    }
+}
