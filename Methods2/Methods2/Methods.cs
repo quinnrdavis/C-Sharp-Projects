bc8b0407dccdac3b2d390306c73d89db5c95023b
Quinn R Davis <quinn.r.davis@gmail.com>
2022-09-26 09:54:52 -0700

    Method overflow assignment

---
+++ Methods2/Methods2/Methods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    class Methods
+    {
+        // method that adds 5 to input
+        public int MathMethod(int i)
+        {
+            return i + 5;
+        }
+
+        // method with the same name that takes a decimal input
+        public int MathMethod(decimal d)
+        {
+            // cast input to int
+            int i = Convert.ToInt32(d);
+
+            return i - 12;
+        }
+
+        // method with the same name that takes a string input
+        public int MathMethod(string s)
+        {
+            // cast input to int if possible
+            int i = Convert.ToInt32(s);
+
+            return i * 4;
+        }
+    }
+}
---
+++ Methods2/Methods2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate an object of methods class
+            Methods methods = new Methods();
+
+            // method with int input
+            Console.WriteLine(methods.MathMethod(5));
+
+            // method with decimal input
+            Console.WriteLine(methods.MathMethod(5.000m));
+
+            // method with string input
+            Console.WriteLine(methods.MathMethod("5"));
+
+            Console.Read();
+        }
+    }
+}
