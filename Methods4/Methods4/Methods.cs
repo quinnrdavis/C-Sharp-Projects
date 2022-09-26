920ad4075fa01005933f8522fb292c2074507bf1
Quinn R Davis <quinn.r.davis@gmail.com>
2022-09-26 10:29:37 -0700

    Void method assignment

---
+++ Methods4/Methods4/Methods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods4
+{
+    class Methods
+    {
+        // void method
+        public void MathMethod(int i, int j)
+        {
+            // perform math operation on first parameter
+            int k = i + 5;
+
+            // print second parameter to the screen
+            Console.WriteLine(j);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Methods4/Methods4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate class
+            Methods methods = new Methods();
+
+            // call method with both parameters
+            methods.MathMethod(4, 6);
+
+            // call method with parameters by name
+            methods.MathMethod(i:5, j:3);
+        }
+    }
+}
