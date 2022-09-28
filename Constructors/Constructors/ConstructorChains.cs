3987ce2a47783345eae18bfd2e9dea6db27b9115
Quinn R Davis <quinn.r.davis@gmail.com>
2022-09-28 15:24:52 -0700

    Constructors assignment

---
+++ Constructors/Constructors/ConstructorChains.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class ConstructorChains
+    {
+        public ConstructorChains() : this("name", 1)
+        {
+        }
+        public ConstructorChains(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // a const variable
+            const string str = "This is a const";
+            Console.WriteLine(str);
+
+            // a variable declared with var
+            var str2 = "This is a var";
+            Console.WriteLine(str2);
+
+            // instantiate class with constructor chain
+            ConstructorChains cc = new ConstructorChains();
+            // print Id property that was assigned by the chained constructor
+            Console.WriteLine(cc.Id);
+
+            Console.Read();
+        }
+    }
+}
