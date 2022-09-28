85427493338cc6e420ea87aff64267750c6be5db
Quinn R Davis <quinn.r.davis@gmail.com>
2022-09-28 09:59:40 -0700

    DateTimes assignment

---
+++ DateTimes/DateTimes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // print current time
+            Console.WriteLine(DateTime.Now);
+
+            // ask user for a number
+            Console.WriteLine("Please enter a number: ");
+            // cast input to int
+            int input = Convert.ToInt32(Console.ReadLine());
+            // add input as hours to current time
+            DateTime dt = DateTime.Now.AddHours(input);
+            // print result
+            Console.WriteLine(dt);
+
+            Console.Read();
+        }
+    }
+}
