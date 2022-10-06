c9d581da5e86e41aa6dd88dea864ef255ddc194e
Quinn R Davis <quinn.r.davis@gmail.com>
2022-10-06 11:06:58 -0700

    Code first database assignment

---
+++ CodeFirstDB/CodeFirstDB/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CodeFirstDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
